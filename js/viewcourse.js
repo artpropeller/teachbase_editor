1a6265a5f9ae05ea8cffbfcf33e813431b101211
Ivan Aleksanov <zloi.gremlin@gmail.com>
2012-10-23 10:55:04 +0400

    view js

---
+++ js/viewcourse.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Ivan
+ * Date: 23.10.12
+ * Time: 10:51
+ * To change this template use File | Settings | File Templates.
+ */
+//-------------------------------- begin -------------------------------------//
+// скрытие панели привью
+
+$(function(){
+
+});
+//-------------------------------- end -------------------------------------//
