7e227fc80d346ea6f6b0b7eb022184cb455db96b
Ivan Aleksanov <zloi.gremlin@gmail.com>
2012-10-27 19:21:34 +0400

    video block

--- js/editor.js
+++ js/editor.js
@@ -171,33 +171,33 @@ $(function () {
 // добавление теста в курс
 
 
-$(function(){
+$(function () {
     // показываем окно выбора
     $('a.new-test').fancybox({
-        beforeShow:function(){
+        beforeShow:function () {
             $('.blank-cover').remove();
             $('.new-course').hide(0);
             $('.create').removeClass('active');
         },
-        afterShow: function(){
+        afterShow:function () {
             // скролл тестов
             $('.test-list .scroll').jScrollPane();
         }
     });
 
     // выделение активного
-    $('.test-list .test').click(function(){
-       if ($(this).is('.active')) {
-           $(this).removeClass('active');
-       }
+    $('.test-list .test').click(function () {
+        if ($(this).is('.active')) {
+            $(this).removeClass('active');
+        }
         else {
-           $('.test-list .test').removeClass('active');
-           $(this).addClass('active');
-       }
+            $('.test-list .test').removeClass('active');
+            $(this).addClass('active');
+        }
     });
 
     // переключение категорий
-    $('.choice-test .categories li a').click(function(){
+    $('.choice-test .categories li a').click(function () {
         $('.choice-test .categories li').removeClass('active');
         $(this).parent().addClass('active');
         showTestCat();
@@ -206,7 +206,7 @@ $(function(){
     });
 
     //показать тесты выбранной категории
-    function showTestCat(){
+    function showTestCat() {
         var cat = $('.choice-test .categories li.active a').attr('rel');
         var tests = $('.test-list .test');
         if (cat == 'all') {
@@ -214,7 +214,7 @@ $(function(){
         }
         else {
             tests.hide(0);
-            tests.each(function(){
+            tests.each(function () {
                 var cats = $(this).attr('cat').split(',');
                 if ($.inArray(cat, cats) + 1) {
                     $(this).show(0);
@@ -225,7 +225,7 @@ $(function(){
 
 
 //    проверка запроса поиска на соответствие списка
-    function checkSearch(t){
+    function checkSearch(t) {
         var text;
         if (t) {
             text = t;
@@ -234,8 +234,8 @@ $(function(){
             text = $('.test-items .search input').val();
         }
         if (text) {
-            $('.test-list .test:visible').each(function(){
-                if ($(this).find('.name').text().toLowerCase().indexOf(text.toLowerCase()) + 1 == 0){
+            $('.test-list .test:visible').each(function () {
+                if ($(this).find('.name').text().toLowerCase().indexOf(text.toLowerCase()) + 1 == 0) {
                     $(this).hide();
                 }
             });
@@ -244,15 +244,16 @@ $(function(){
     }
 
 //живой поиск
-    $('.test-items .search input').keydown(function(event) {
+    $('.test-items .search input').keydown(function (event) {
         var text;
-        setTimeout(function(){text = $('.test-items .search input').val();}, 50);
-        setTimeout(function() {
+        setTimeout(function () {
+            text = $('.test-items .search input').val();
+        }, 50);
+        setTimeout(function () {
             showTestCat();
             checkSearch(text);
         }, 150);
     });
-
 
 
 });
@@ -260,12 +261,12 @@ $(function(){
 //-------------------------------- begin -------------------------------------//
 // просмотр теста
 
-$(function(){
+$(function () {
     $('.test-view label').click(
-        function(){
+        function () {
             $(this).toggleClass('active');
             if ($(this).is('#timetest')) {
-                if ($(this).is('.active')){
+                if ($(this).is('.active')) {
                     $('#time-deadline').show();
                 }
                 else {
@@ -277,61 +278,74 @@ $(function(){
 
     //баллы проценты
 
-    $('#setbp .inparr .up').click(function() {
+    $('#setbp .inparr .up').click(function () {
         var inp = $(this).parent().find('input');
         var val = parseInt(inp.val()) + 1;
         if (val <= parseInt(inp.attr('max'))) {
-            inp.val(parseInt(inp.val()) + 1);}
-        if ($(this).parent().find('input').is('#percent')) {changeBall();} else {changePercent();}
+            inp.val(parseInt(inp.val()) + 1);
+        }
+        if ($(this).parent().find('input').is('#percent')) {
+            changeBall();
+        } else {
+            changePercent();
+        }
         return false;
     });
 
-    $('#setbp .inparr input').change(function(){
+    $('#setbp .inparr input').change(function () {
         if (parseInt($(this).val()) > parseInt($(this).attr('max'))) {
             $(this).val($(this).attr('max'));
         }
     });
 
-    $('#setbp #percent').bind('change keyup', function(){
-        if (!$(this).val()){$(this).val('0');}
+    $('#setbp #percent').bind('change keyup', function () {
+        if (!$(this).val()) {
+            $(this).val('0');
+        }
         changeBall();
     });
 
-    $('#setbp #ball').bind('change keyup', function(){
-        if (!$(this).val()){$(this).val('0');}
+    $('#setbp #ball').bind('change keyup', function () {
+        if (!$(this).val()) {
+            $(this).val('0');
+        }
         changePercent();
     });
 
-    function changeBall(){
-        var i = parseFloat($('#ball').attr('max'))/100;
+    function changeBall() {
+        var i = parseFloat($('#ball').attr('max')) / 100;
         if (i) {
-            $('#ball').val(parseInt(i*parseInt($('#percent').val())));
+            $('#ball').val(parseInt(i * parseInt($('#percent').val())));
         }
         else {
             $('#ball').val(0);
         }
     }
 
-    function changePercent(){
-        var i = parseFloat($('#ball').val()/$('#ball').attr('max'))*100;
+    function changePercent() {
+        var i = parseFloat($('#ball').val() / $('#ball').attr('max')) * 100;
         if (i) {
-        $('#percent').val(parseInt(i));
+            $('#percent').val(parseInt(i));
         }
         else {
             $('#percent').val(0);
         }
     }
 
-    $('#setbp .inparr input').keyup(function(e){
+    $('#setbp .inparr input').keyup(function (e) {
         if (parseInt($(this).val()) > parseInt($(this).attr('max'))) {
             $(this).val($(this).attr('max'));
         }
-        if ($(this).is('#percent')) {changeBall();} else {changePercent();}
+        if ($(this).is('#percent')) {
+            changeBall();
+        } else {
+            changePercent();
+        }
     });
 
-    $('#setbp .inparr input').keydown(function(event) {
+    $('#setbp .inparr input').keydown(function (event) {
         console.log(event);
-        if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 ||
+        if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 ||
             // Allow: Ctrl+A
             (event.keyCode == 65 && event.ctrlKey === true) ||
             // Allow: home, end, left, right
@@ -347,18 +361,21 @@ $(function(){
         }
     });
 
-    $('#setbp .inparr .down').click(function() {
+    $('#setbp .inparr .down').click(function () {
         $(this).parent().find('input').val(parseInt($(this).parent().find('input').val()) - 1);
-        if ($(this).parent().find('input').is('#percent')) {changeBall();} else {changePercent();}
+        if ($(this).parent().find('input').is('#percent')) {
+            changeBall();
+        } else {
+            changePercent();
+        }
         return false;
 
     });
 
 
-    $('.fullsettings').click(function(){
+    $('.fullsettings').click(function () {
         $('.blockset').toggle(0);
     });
-
 
 
 });
@@ -366,51 +383,153 @@ $(function(){
 //-------------------------------- begin -------------------------------------//
 // добавление таблиц
 
-$(function(){
+$(function () {
 
 
-
-    $(".insert-table .mouse").mousemove(function(event) {
+    $(".insert-table .mouse").mousemove(function (event) {
         var hl = $('.insert-table .highlight');
         var df = $('.insert-table .cells_container, .insert-table .unhighlight');
 //        console.log(event.pageY - document.getElementById('inserttable').offsetTop);
         var height, width;
-        if(typeof event.offsetX === "undefined" || typeof event.offsetY === "undefined") {
+        if (typeof event.offsetX === "undefined" || typeof event.offsetY === "undefined") {
             var targetOffset = $(event.target).offset();
             event.offsetX = event.pageX - targetOffset.left;
             event.offsetY = event.pageY - targetOffset.top;
         }
-        height = Math.ceil((event.offsetY)/20);
-        width = Math.ceil((event.offsetX)/20);
+        height = Math.ceil((event.offsetY) / 20);
+        width = Math.ceil((event.offsetX) / 20);
         $('.insert-table .status .x').html(width);
         $('.insert-table .status .y').html(height);
-        hl.css({'width':width*20+'px','height':height*20+'px'});
+        hl.css({'width':width * 20 + 'px', 'height':height * 20 + 'px'});
         if (height >= 4 && height <= 9) {
-            df.css({'height':(height+1)*20+'px'});
+            df.css({'height':(height + 1) * 20 + 'px'});
         }
         else {
-            if (height <= 5) {df.css({'height':(5*20)+'px'});}
-            if (height == 10) {df.css({'height':(10*20)+'px'});}
+            if (height <= 5) {
+                df.css({'height':(5 * 20) + 'px'});
+            }
+            if (height == 10) {
+                df.css({'height':(10 * 20) + 'px'});
+            }
         }
         if (width >= 4 && width <= 9) {
-            df.css({'width':(width+1)*20+'px'});
+            df.css({'width':(width + 1) * 20 + 'px'});
         }
         else {
-            if (width <= 5) {df.css({'width':(5*20)+'px'});}
-            if (width == 10) {df.css({'width':(10*20)+'px'});}
+            if (width <= 5) {
+                df.css({'width':(5 * 20) + 'px'});
+            }
+            if (width == 10) {
+                df.css({'width':(10 * 20) + 'px'});
+            }
         }
     });
 
-    $(".insert-table .mouse").click(function(){
-        alert("Размер: "+$('.insert-table .status .x').html()+' x ' + $('.insert-table .status .y').html());
+    $(".insert-table .mouse").click(function () {
+        alert("Размер: " + $('.insert-table .status .x').html() + ' x ' + $('.insert-table .status .y').html());
     });
 
 });
 
 
-
 //-------------------------------- begin -------------------------------------//
-//
+// редактирование видео
+
+$(function () {
+
+    //редактирование текста по клику на строку "placeholder"
+    $('.videoblock .placeholder').click(function () {
+        $(this).hide(0);
+        $(this).next().show();
+        $(this).next().focus();
+    });
+
+    //удаление видео
+    $('.videoblock .del').click(function () {
+        $(this).parents('.videoblock').remove();
+    });
+
+
+    //установка высоты картинки привью видео
+    $('.videoblock').each(function () {
+        $(this).find('.video').css({'height':$(this).height() - ($(this).find('.header').height()+$(this).find('.description').height())});
+    });
+
+    //активация редактирования видеоблока
+    $('.videoblock').click(function () {
+        if (!$(this).is('.active')) {
+            $(this).addClass('active');
+            var body = $('body');
+            body.prepend('<div class="wrap-video"></div>');
+            body.addClass('no-select');
+            $(this).css('height',($(this).find('.video').height() + 80) + 'px');
+            $(this).find('.header').css('height','30px');
+            $(this).find('.description').css('height','50px');
+            var name = $(this).find('.header .name');
+            if (name.html()) {
+                name.hide(0);
+                name.prev().show(0).html(name.text());
+            }
+            else {
+                name.prev().prev().show(0);
+            }
+            var desc = $(this).find('.description .desc');
+            if (desc.html()) {
+                desc.hide(0);
+                desc.prev().show(0).html(desc.text());
+            }
+            else {
+                desc.prev().prev().show(0);
+            }
+            $('.videoblock input:visible,.videoblock textarea:visible').each(function(){
+               $(this).prev().hide(0);
+            });
+            $(this).resizable({
+                maxHeight:383,
+                maxWidth:453,
+                minHeight:218,
+                minWidth:245,
+                resize:function (e, el) {
+                    var ar = $(el.originalElement);
+                    ar.find('.video').css({'height':el.size['height'] - 80});
+                }
+            });
+        }
+    });
+
+    //отключение редактирования видеоблока
+    $('.wrap-video').live('click', function(){
+        var item = $('.videoblock');
+        var name = item.find('.header input');
+        name.hide(0);
+        name.next().html(name.val());
+        if (name.val()){
+            name.next().show(0);
+        }
+        else {
+            name.prev().hide(0);
+            name.next().hide(0);
+            item.find('.header').css('height','6px');
+            item.css('height',(item.height() - 24) + 'px');
+        }
+        var desc = item.find('textarea');
+        desc.hide(0);
+        desc.next().html(desc.val());
+        console.log(desc.val());
+        if (desc.val()){
+            desc.next().show(0);
+        }
+        else {
+            desc.prev().hide(0);
+            desc.next().hide(0);
+            desc.find('.description').css('height','0');
+            item.css('height',(item.height() - 43) + 'px');
+        }
+        item.removeClass('active');
+        $('body').removeClass('no-select');
+        $(this).remove();
+    });
+});
 
 
 //-------------------------------- end -------------------------------------//
